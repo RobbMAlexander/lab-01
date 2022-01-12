8f4830604a4d7507e1e753fe8d76e0054266dd9f
Robb Alexander <RobbMAlexander@gmail.com>
2022-01-11 20:39:36 -0800

    lab 2 completed w/ some styling

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 import HornedBeast from './HornedBeast.js';
+import './Main.css';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class Main extends React.Component {
   render () {
+
+      let beastPaddock = this.props.data.map((beast, idx) => (
+       
+        <HornedBeast
+        key={idx}
+        name={beast.title}
+        imageUrl={beast.image_url}
+        hornCount={beast.horns}
+        description={beast.description}
+        keyword={beast.keyword}
+        title={beast.title}
+        />
+     
+      ));
+
+
+
     return (
       <>
-        <HornedBeast name="HB1" imageUrl="srcurl"/>
-        <HornedBeast name="HB2" imageUrl="srcurl"/>
+        {beastPaddock}  
       </>
     )
   }
