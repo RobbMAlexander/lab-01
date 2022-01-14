7704d89c02a9f56ddbdf69da3e483596a3709d4f
Robb Alexander <RobbMAlexander@gmail.com>
2022-01-13 23:43:28 -0800

    WIP: form added and outputting state change, but re-rendering not properly wired

--- src/App.js
+++ src/App.js
@@ -4,65 +4,85 @@ import Main from './Main.js';
 import Footer from './Footer.js';
 import data from './data.json';
 import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button'
-import { CardImg, ModalBody } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: false,
+      zoomed: false,
       image: 'placeholder',
-      blurb: 'Lorem Ipsum'
+      blurb: 'Lorem Ipsum',
+      cardDisplayed: true,
+      hornsSelectedState: 'all',
     }
   }
 
-
   handleRenderModal = (imageUrl, description) => {
     this.setState({
-      selected: true,
+      zoomed: true,
       image: imageUrl,
       blurb: description,
+
     })
   }
 
   handleRemoveModal = () => {
     this.setState({
-      selected: false
+      zoomed: false
     })
   }
 
-  render() {
-    return (
-      <>
-        <Header />
+  handleSelectForm = e => {
+    e.preventDefault();
+    let hornsSelected = e.target.value;
+    this.setState({
+      hornsSelectedState: hornsSelected
+    })
+}
 
-        <Main 
-        data={data} 
-        handleRenderModal={this.handleRenderModal}
-        />
+render() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <Form style={{ width: '15%', margin: 'auto' }}>
+          <Form.Group controlId="selected">
+            <Form.Label style={{ textAlign: "center" }}>
+              Sort By Horn Count
+            </Form.Label>
+            <Form.Select name="hornsSelected" id="hornsSelected" onChange={this.handleSelectForm}>
+              <option value="all">All</option>
+              <option value="1">1 Horn</option>
+              <option value="2">2 Horns</option>
+              <option value="3">3 Horns</option>
+              <option value="many">4+ Horns</option>
+            </Form.Select>
+          </Form.Group>
+        </Form>
+      </Container>
+      <Main
+        data={data}
+        handleRenderModal={this.handleRenderModal} />
+      <Modal contentClassName="modal-content" size="lg" centered show={this.state.zoomed} onHide={this.handleRemoveModal}>
+        <Modal.Header>
+          <Modal.Title></Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img className="zoomed" align="center" width="100%" margin="auto"
+            src={this.state.image}
+            alt={this.state.description}
+          />
+          <p className="blurb">{this.state.blurb}</p>
+        </Modal.Body>
+      </Modal>
 
-        <Modal centered show={this.state.selected} onHide={this.handleRemoveModal}>
-          <Modal.Header>
-            <Modal.Title></Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <img width="130%" className="zoomed"
-              src={this.state.image}
-              alt={this.state.description}
-            />
-            {/* <Button variant="primary" 
-            onClick={this.handleVote}>
-              Vote
-            </Button> */}
-            {this.state.blurb}
-          </Modal.Body>
-        </Modal>
-
-        <Footer />
-      </>
-    );
-  }
+      <Footer />
+    </>
+  );
+}
 }
 
 export default App;
